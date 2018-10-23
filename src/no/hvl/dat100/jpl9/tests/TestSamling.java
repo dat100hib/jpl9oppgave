737b87824d771a993c35c02f9038cdc25d40bb30
Lars Michael Kristensen <lmkr@hvl.no>
2018-10-23 23:23:09 +0200

    utvidet test for filer og samling

--- src/no/hvl/dat100/jpl9/tests/TestSamling.java
+++ src/no/hvl/dat100/jpl9/tests/TestSamling.java
@@ -11,8 +11,8 @@ public class TestSamling {
 	public void testStandardConstructor() {
 		PersonSamling samling = new PersonSamling();
 		
-		assertEquals(samling.getAntall(),0);
-		assertEquals(samling.getSamling().length,20);
+		assertEquals(0,samling.getAntall());
+		assertEquals(20,samling.getSamling().length);
 		
 	}
 
@@ -20,22 +20,65 @@ public class TestSamling {
 	public void testConstructor() {
 		PersonSamling samling = new PersonSamling(40);
 		
-		assertEquals(samling.getAntall(),0);
-		assertEquals(samling.getSamling().length,40);
+		assertEquals(0,samling.getAntall());
+		assertEquals(40,samling.getSamling().length);
 	}
 	
-	// TODO: 
-	// finnes
-	// ledig plass
-	// enkel leggTil
-	// utvid
-	// så avansert legg til
-	
-	@Test 
-	public void testleggTil() {
+	@Test
+	public void testfinnPerson() {
 		PersonSamling samling = new PersonSamling(40);
 		
 		Student student1 = new Student("Olsen", "Ole", 6,100,"1A");
+		Student student2 = new Student("Olsen", "Oline", 7,101,"1A");
+		Student student3 = new Student("Olsen", "Oline", 8,102,"1A");
+		
+		Person[] tabell = samling.getSamling();
+		
+		assertEquals(-1,samling.finnPerson(student1));
+		
+		tabell[0] = student1;
+		tabell[1] = student2;
+		
+		samling.setAntall(2);
+		
+		assertEquals(0,samling.finnPerson(student1));
+		assertEquals(1,samling.finnPerson(student2));
+		assertEquals(-1,samling.finnPerson(student3));
+		
+		
+	}
+	
+	@Test
+	public void testfinnes() {
+		PersonSamling samling = new PersonSamling(40);
+		
+		Student student1 = new Student("Olsen", "Ole", 6,100,"1A");
+		Student student2 = new Student("Olsen", "Oline", 7,101,"1A");
+		Student student3 = new Student("Olsen", "Oline", 8,102,"1A");
+		
+		Person[] tabell = samling.getSamling();
+		
+		assertFalse(samling.finnes(student1));
+		
+		tabell[0] = student1;
+		tabell[1] = student2;
+		
+		samling.setAntall(2);
+		
+		assertTrue(samling.finnes(student1));
+		assertTrue(samling.finnes(student2));
+		assertFalse(samling.finnes(student3));
+		
+		
+	}
+	
+	@Test 
+	public void testleggTil() {
+		
+		PersonSamling samling = new PersonSamling(2);
+		
+		Student student1 = new Student("Olsen", "Ole", 6,100,"1A");
+		
 		Student student2 = new Student("Olsen", "Oline", 7,101,"1A");
 		
 		Laerer laerer = new Laerer("Olsen", "Ole", 8,1000,7676);
@@ -43,54 +86,92 @@ public class TestSamling {
 		assertTrue(samling.leggTil(student1));
 		assertTrue(samling.leggTil(laerer));
 		
-		assertEquals(samling.getAntall(),2);
-		
-		assertTrue(samling.finnPerson(student1) >= 0);
-		assertTrue(samling.finnPerson(student2) < 0);
+		assertEquals(2,samling.getAntall());
 		
 		assertTrue(samling.finnes(student1));
 		assertTrue(samling.finnes(laerer));
+		
+		assertFalse(samling.leggTil(student2));
+				
+		assertEquals(2,samling.getAntall());	
+		
+		assertTrue(samling.finnes(student1));
+		assertTrue(samling.finnes(laerer));
+		
 		assertFalse(samling.finnes(student2));
 		
 	}
 	
 	@Test
-	public void testledigPlass() {
-		PersonSamling samling1 = new PersonSamling(40);
-		PersonSamling samling2 = new PersonSamling(2);
-		
-		Student student = new Student("Olsen", "Ole", 6,100,"1A");
-		Laerer laerer = new Laerer("Olsen", "Ole", 8,1000,7676);
-		
-		samling1.leggTil(student);
-		samling1.leggTil(laerer);
-		
-		samling2.leggTil(student);
-		samling2.leggTil(laerer);
-		
-		assertTrue(samling1.ledigPlass());
-		assertFalse(samling2.ledigPlass());
-	}
-	
-	@Test
 	public void testutvid() {
+		
 		PersonSamling samling = new PersonSamling(2);
+		Student student1 = new Student("Olsen", "Ole", 6,100,"1A");
+		Student student2 = new Student("Olsen", "Oline", 7,101,"1A");
 		
-		Student student = new Student("Olsen", "Ole", 6,100,"1A");
-		Laerer laerer = new Laerer("Olsen", "Ole", 8,1000,7676);
-
-		samling.leggTil(student);
-		samling.leggTil(laerer);
-		
-		assertEquals(samling.getAntall(),2);
-		assertFalse(samling.ledigPlass());
+		samling.leggTil(student1);
+		samling.leggTil(student2);
 		
 		samling.utvid();
 		
-		assertTrue(samling.finnes(student));
+		assertEquals(2,samling.getAntall());
+		assertTrue(samling.finnes(student1));
+		assertTrue(samling.finnes(student2));
+		
+		assertEquals(4, samling.getSamling().length);
+		
+	}
+	
+	@Test 
+	public void testleggTilUtvid() {
+		PersonSamling samling = new PersonSamling(2);
+		
+		Student student1 = new Student("Olsen", "Ole", 6,100,"1A");
+		Student student2 = new Student("Olsen", "Oline", 7,101,"1A");
+		
+		Laerer laerer = new Laerer("Olsen", "Ole", 8,1000,7676);
+		
+		assertTrue(samling.leggTilUtvid(student1));
+		assertTrue(samling.leggTilUtvid(student2));
+		
+		assertEquals(2,samling.getAntall());
+		
+		assertTrue(samling.finnes(student1));
+		assertTrue(samling.finnes(student2));
+		assertFalse(samling.ledigPlass());
+		
+		assertTrue(samling.leggTilUtvid(laerer));
+		
+		assertEquals(3,samling.getAntall());
+		assertEquals(4,samling.getSamling().length);
+		assertTrue(samling.ledigPlass());
+		
+		assertTrue(samling.finnes(student1));
 		assertTrue(samling.finnes(laerer));
-		assertEquals(samling.getAntall(),2);
-		assertTrue(samling.ledigPlass());	
+		assertTrue(samling.finnes(student2));
+		
+	}
+	
+	@Test
+	public void testledigPlass() {
+		
+		PersonSamling samling = new PersonSamling(2);
+		
+		Student student1 = new Student("Olsen", "Ole", 6,100,"1A");
+		Student student2 = new Student("Olsen", "Oline", 7,101,"1A");
+		
+		assertTrue(samling.ledigPlass());
+		
+		Person[] tabell = samling.getSamling();
+		
+		tabell[0] = student1;
+		samling.setAntall(1);
+		assertTrue(samling.ledigPlass());
+		
+		tabell[1] = student2;
+		samling.setAntall(2);
+		assertFalse(samling.ledigPlass());
+
 	}
 	
 	@Test
